13088b5befbb50d50efe954cda59f07930185216
Cadu <cadumega@outlook.com>
2021-08-18 01:26:33 -0300

    arrays

---
+++ script06.js
@@ -0,0 +1,27 @@
+// Armazenar vários carros diferentes
+let carro1 = 'Palio';
+let carro2 = 'Corolla';
+let carro3 = 'Civic';
+let carro4 = 'Mercedes';
+
+
+
+let carros = ['Palio','Corolla','Civic','Mercedes']
+
+let carrosNovos = [
+  'Ferrari',
+  'BMW',
+  'Audi',
+  'Mercedes'
+];
+
+let ingredientes = [
+  ['uva', 'pera', 'maça']            // 2 arrays, uma com 3 itens e outra com 2 itens
+  ['arroz', 'feijão]
+];
+
+log(ingredientes[1][0]);   //arroz
+
+console.log(carros[0]);   //acessar primeira propriedade da array
+
+
