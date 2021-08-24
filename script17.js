04262c18afc68883daac2541ad3347e0a7eb4d75
Cadu <cadumega@outlook.com>
2021-08-23 23:48:08 -0300

    Intervalos

--- script17.js
+++ script17.js
@@ -4,6 +4,24 @@ Ou esperar depois de um tempo e rodar.
 Qualquer tipo de evento que executa simultânea vezes ou retarda.
 */
 
+let timer;
+
+//fç de controles
+function comecar() {
+     timer = setInterval(showTime, 10);
+}
+
+function parar() {
+    clearInterval(timer);       // Limpar intervalo, e passo o parametro a var do timer. Preciso colocar a var no contexto global e não local do contexto interno da fç acima.
+                                // Posso usar agora o timer tanto no comecar quanto no parar.
+}
+
+function rodar() {
+    setTimeout(function() {
+        document.querySelector
+    })
+}
+
 //fç criada, exibir hora atual, com hora minuto e segundo na tela.
 function showTime(){
     let d = new Date();
@@ -17,6 +35,6 @@ function showTime(){
 
 // Criando um timer, com intervalo conforme os parametros q escolhi.
 
-let timer = setInterval(showTime,1000);
+// let timer = setInterval(showTime,1000); // IRA PARA DENTRO DA FC COMECAR
 
 // 6:30 parei
