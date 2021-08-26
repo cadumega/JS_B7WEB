517a669a56b2c3fb25d9717cd42620092f057621
Cadu <cadumega@outlook.com>
2021-08-26 01:03:23 -0300

    includesRepeat

---
+++ script26.js
@@ -0,0 +1,18 @@
+//Funções Include (serve tanta para string quanto para array) e Repeat
+
+let lista = ['ovo', 'café', 'arroz', 'feijão', 'macarrão'];
+
+console.log(lista.includes('carne'));    // false , não tem carne na array, 
+
+
+let nome = 'Carlos';
+
+console.log(nome.includes('a'));     //true , achou difere de minusculo e Maisculo , procura facilmente com include
+
+// Mais prático com include
+// Outras maneiras de achar algo com: Find index ou index off para achar determinado item
+
+
+console.log('x'.repeat(20));          // Bastamte utilizado para quebrar campos, prototipando varios sistemas, fazer teste de quantos caracteres o campo irá aguentar.
+
+
