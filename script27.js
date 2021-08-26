487a4abddc3cf6854a23638aabb585329c34a810
Cadu <cadumega@outlook.com>
2021-08-26 01:15:28 -0300

    Objeto: Key, Values e Entries

---
+++ script27.js
@@ -0,0 +1,25 @@
+//Funciona tanto para obj quanto para array, ambos são objetos
+
+let lista =['ovo' , 'macarrao' ,'feijao' , 'pipoca'];
+
+console.log(Object.keys(lista));        //Indices dessa array
+console.log(Object.values(lista));        //Values dessa array
+console.log(Object.entries(lista));        //I
+
+
+let p ='____________'
+console.log(p);
+
+
+let pessoa = {
+  nome:'Carlos',
+  sobrenome:'Lopes',
+  idade:99
+};
+
+console.log(Object.keys(pessoa));
+console.log(Object.values(pessoa));
+console.log(Object.entries(pessoa));
+
+
+// Uma das opções para ler o objeto e Debugar o código , ver os valores entradas indices....
